3899ddc22973cb3face453e28b0669ec1ebc56be
Hetal1983 <hetalpatel2023.katargam@gmail.com>
2024-04-02 10:07:04 +0530

    added 2 file,map,factorial

---
+++ Factorial_Function.py
@@ -0,0 +1,34 @@
+# ther are two types of function
+# built in
+# user define ??
+
+# def myFun(n):
+#     for i in range(1,11):
+#         print(n,"*",i,"=",n*i)
+
+# a = int(input("N = "))
+
+# myFun(a)
+# myFun(9)
+
+# def myMin(a,b):
+#     # if a<b:
+#     #     return a
+#     # else:
+#         # return b
+#     return a if(a<b) els
+
+# a = myMin(20,45)
+
+# print(myMin(20,45))
+# def fact(n):
+#     m=1
+#     for i in range(1,n+1):
+
+#         m = m * i
+#         print(m)
+#     return m
+# print(fact(4))
+
+
+
---
+++ map.py
@@ -0,0 +1,48 @@
+# ll = [10,20,30,40]
+
+# def fun(a):
+#     return a+5 
+
+# l = tuple(map(fun,ll))
+
+# print(l)
+
+# S1=[20,15,30,60]
+
+# def fun(a):
+#     return a-10
+# b=tuple(map(fun,S1))
+# print(b)
+
+# ll= {10,10,60,90,150}
+
+# def fun(a):
+#     return a*5
+# l=set(map(fun,ll))
+# print(l)
+
+# ll= [10,10,60,90,150]
+
+# def fun(a):
+#     return a*5
+# l=tuple(map(fun,ll))
+# print(l)  
+
+ll= [10,10,60,90,150]
+
+def fun(a):
+    return a%5
+l=tuple(map(fun,ll))
+print(l)
+
+ll= [10,10,60,90,150]
+
+def fun(a):
+    return a/5
+l=set (map(fun,ll))
+print(l)
+
+
+
+
+
